22808b36a3d90111d804c9539c8c1d4a77e3c6ac
Matt1479 <gitmatt1479@gmail.com>
2025-07-30 17:47:24 +0200

    Feature/refactor(app.py): Use session to globally store boggle game content; Implement API for generating content and getting words in board.

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, session
+from flask_session import Session
 import boggle
 import helpers
 
 # Configure application
 app = Flask(__name__)
 
+# Configure session (store on disk)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 # Words in dictionary (of length >= 3)
 G_WORDS = boggle.load("static/dictionaries/large")
-
-# Boggle game content
-G_CONTENT = {}
 
 
 @app.after_request
@@ -24,11 +27,30 @@ def after_request(response):
 
 @app.route("/")
 def index():
+    return render_template("index.html")
 
-    # Generate board
-    content = helpers.generate_board(3, 3)
 
-    return render_template("index.html", content=content)
+@app.route("/api/generate-content-<rows>-<cols>")
+def api_generate_content(rows, cols):
+    try:
+        if (rows and cols) and rows == cols:
+            session["content"] = helpers.generate_board(int(rows), int(cols))
+        else:
+            session["content"] = {}
+    except ValueError:
+        return jsonify({"Error": "rows and cols must be equal and an integer."})
+    
+    return jsonify(session["content"])
+
+
+@app.route("/api/get-words-in-board")
+def api_get_words_in_board():
+    if not session["content"]:
+        return jsonify({"Error": "No content."})
+    
+    return jsonify(boggle.word_boggle(
+        session["content"]["board"], session["content"]["rows"],
+        session["content"]["cols"], G_WORDS))
 
 
 @app.route("/dictionary")
