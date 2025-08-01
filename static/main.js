4a081963f558f38cc551903fcf3037feb1149ab2
Matt1479 <gitmatt1479@gmail.com>
2025-08-01 13:24:12 +0200

    Feature(main.js): Allow deselection only of the last selected letter; Allow selection only of adjacent letters.

--- static/main.js
+++ static/main.js
@@ -31,21 +31,47 @@ async function main() {
                     'border', 'letter');
                 spanCol.textContent = content['board'][i][j];
 
+                spanCol.dataset.row = i;
+                spanCol.dataset.col = j;
+
                 // Listen for click event on grid cells; toggle 'selected' class on click
                 spanCol.addEventListener('click', () => {
-                    spanCol.classList.toggle('selected');
-                    if (spanCol.classList.contains('selected')) {
-                        selectedElems.push(spanCol);
+
+                    const alreadySelected = spanCol.classList.contains('selected');
+
+                    if (alreadySelected) {
+                        // Allow deselection only of the last selected
+                        if (selectedElems[selectedElems.length - 1] == spanCol) {
+                            spanCol.classList.remove('selected');
+                            selectedElems.pop();
+                        } else {
+                            document.querySelector('#result').innerHTML =
+                            `<p class="fw-bold text-info">
+                                You can only only deselect the last selected letter.
+                            </p>`;
+                        }
                     } else {
-                        selectedElems.splice(selectedElems.indexOf(spanCol), 1);
+                        // First click is allowed unconditionally
+                        if (selectedElems.length == 0) {
+                            spanCol.classList.add('selected');
+                            selectedElems.push(spanCol);
+                        } else {
+                            const lastSelected = selectedElems[selectedElems.length - 1];
+                            if (isAdjacent(spanCol, lastSelected)) {
+                                spanCol.classList.add('selected');
+                                selectedElems.push(spanCol);
+                            } else {
+                                // Not adjacent
+                                document.querySelector('#result').innerHTML =
+                                `<p class="fw-bold text-info">
+                                    You can only only select adjacent letters.
+                                </p>`;
+                            }
+                        }
                     }
 
-                    // Enable checking once we have 3 or more elements selected
-                    if (selectedElems.length >= 3) {
-                        document.querySelector('#checkWordBtn').disabled = false;
-                    } else {
-                        document.querySelector('#checkWordBtn').disabled = true;
-                    }
+                    // Enable/disable check button
+                    document.querySelector('#checkWordBtn').disabled = selectedElems.length < 3;
                 });
                 
                 divRow.appendChild(spanCol);
@@ -103,7 +129,7 @@ async function main() {
             }
         } else {
             document.querySelector('#result').innerHTML =
-            `<p class="fw-bold text-danger">
+            `<p class="fw-bold text-info">
                 I'm afraid there is no such word as ${word} in this board.
             </p>`;
         }
@@ -144,6 +170,20 @@ async function getWordsInBoardFromAPI() {
     } catch (error) {
         console.error(error.message);
     }
+}
+
+function isAdjacent(a, b) {
+    const rowA = parseInt(a.dataset.row);
+    const colA = parseInt(a.dataset.col);
+    
+    const rowB = parseInt(b.dataset.row);
+    const colB = parseInt(b.dataset.col);
+
+    const rowDiff = Math.abs(rowA - rowB);
+    const colDiff = Math.abs(colA - colB);
+
+    // Adjacent only if rowDiff <= 1 and colDiff <= 1 and they're not equal
+    return rowDiff <= 1 && colDiff <= 1 && !(rowA == rowB && colA == colB);
 }
 
 document.addEventListener('DOMContentLoaded', main);
